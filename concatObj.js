b916e65eb391d2e510302594ac5cb8870023dab5
angelyaaaaa <tsoen.526018@gmail.com>
2016-12-27 01:27:49 +0800

    fix writing mistake

--- concatObj.js
+++ concatObj.js
@@ -1,13 +1,12 @@
 module.exports = function() {
-    var outpu = {};
-	    var len = arguments.length;
-	    for (var i = 0; i < len; i++) {
-	        for (key in arguments[i]) {
-	            if (arguments[i].hasOwnProperty(key)) {
-	                output[key] = arguments[i][key];
-	            }
-	        }
-	    }
-	    return output;
-	}	
-}
+	var output = {};
+	var len = arguments.length;
+	for (var i = 0; i < len; i++) {
+		for (let key in arguments[i]) {
+			if (arguments[i].hasOwnProperty(key)) {
+				output[key] = arguments[i][key];
+			}
+		}
+	}
+	return output;
+};
